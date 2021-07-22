16d653880256042b16b2e0be4ffc8dbf95d58cf4
arieltecay <arieltecay@gmail.com>
2021-07-22 20:40:03 -0300

    changes created of entire code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,66 @@
-import { BrowserRouter, Route } from "react-router-dom";
-import HomeScreen from "./screens/HomeScreen";
-import ProductScreen from "./screens/ProductScreen";
+import data from "./data";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <div className="grid-container">
-          <header className="row">
-            <div>
-              <a className="brand" href="index.html">
-                E-Commerce
-              </a>
+    <div>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">
+              E-Commerce
+            </a>
+          </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <div>
+            <div className="row center">
+              {data.products.map((prod) => (
+                <div key={prod._id} className="card">
+                  <a href={`/product/${prod._id}`}>
+                    <img className="medium" src={prod.image} alt={prod.name} />
+                  </a>
+                  <div className="card-body">
+                    <a href="product.html">
+                      <h2>{prod.name}</h2>
+                    </a>
+                    <div className="rating">
+                      <span>
+                        {" "}
+                        <i className="fa fa-star"></i>{" "}
+                      </span>
+                      <span>
+                        {" "}
+                        <i className="fa fa-star"></i>{" "}
+                      </span>
+                      <span>
+                        {" "}
+                        <i className="fa fa-star"></i>{" "}
+                      </span>
+                      <span>
+                        {" "}
+                        <i className="fa fa-star"></i>{" "}
+                      </span>
+                      <span>
+                        {" "}
+                        <i className="fa fa-star"></i>{" "}
+                      </span>
+                    </div>
+                    <div className="price">${prod.price}</div>
+                  </div>
+                </div>
+              ))}
             </div>
-            <div>
-              <a href="cart.html">Cart</a>
-              <a href="signin.html">Sign In</a>
-            </div>
-          </header>
-          <main>
-            <Route path="/product/:id" component={ProductScreen}></Route>
-            <Route path="/" component={HomeScreen} exact></Route>
-          </main>
-          <footer className="row center">
-            All Rigth Reserved for company Ariel Tecay
-          </footer>
-        </div>
+          </div>
+        </main>
+        <footer className="row center">
+          All right reserved For Ariel Tecay
+        </footer>
       </div>
-    </BrowserRouter>
+    </div>
   );
 }
 
