7d0cbc572b24235f45d8fe68c11f7cfba58b62c1
arieltecay <arieltecay@gmail.com>
2021-07-28 22:36:44 -0300

    Add redux to the product screen

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,25 +1,23 @@
 import React from "react";
 import Rating from "./Rating";
 
-const Product = ({ product, keyProp }) => {
+export default function Product(props) {
+  const { product } = props;
   return (
-    <div key={keyProp}>
-      <div className="card">
+    <div key={product._id} className="card">
+      <a href={`/product/${product._id}`}>
+        <img className="medium" src={product.image} alt={product.name} />
+      </a>
+      <div className="card-body">
         <a href={`/product/${product._id}`}>
-          <img className="medium" src={product.image} alt={product.name} />
+          <h2>{product.name}</h2>
         </a>
-        <div className="card-body">
-          <a href="/product">
-            <h2>{product.name}</h2>
-          </a>
-          <div className="rating">
-            <Rating  rating={product.rating} numReviews={product.numReviews}/>
-          </div>
-          <div className="price">${product.price}</div>
-        </div>
+        <Rating
+          rating={product.rating}
+          numReviews={product.numReviews}
+        ></Rating>
+        <div className="price">${product.price}</div>
       </div>
     </div>
   );
-};
-
-export default Product;
+}
