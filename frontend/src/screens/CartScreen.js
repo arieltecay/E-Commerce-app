e79ced3bd27da7d3c32bd8714ca97a0bf9e4a04a
arieltecay <arieltecay@gmail.com>
2021-07-29 22:32:57 -0300

    Add products to cart and watch it working

---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const CartScreen = (props) => {
+  const productId = props.match.params.id;
+  const qty = props.location.search
+    ? Number(props.location.search.split("=")[1])
+    : 1;
+  console.log(qty);
+  return (
+    <div>
+      <h1>Cart Screen</h1>
+      <p>
+        Add to Cart : ProductID: {productId} Qty: {qty}
+      </p>
+    </div>
+  );
+};
