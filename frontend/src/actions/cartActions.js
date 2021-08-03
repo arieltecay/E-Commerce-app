d8511a6d05ccabafd16713b779c9268dd6f0ae8b
arieltecay <arieltecay@gmail.com>
2021-08-02 22:53:20 -0300

    make it work dellete function in cartScreen

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM } from "../constants/CartConstants";
+import { CART_ADD_ITEM,CART_REMOVE_ITEM } from "../constants/CartConstants";
 import Axios from "axios";
 
 export const addToCart = (productId, qty) => async (dispatch, getState) => {
@@ -14,5 +14,10 @@ export const addToCart = (productId, qty) => async (dispatch, getState) => {
       qty,
     },
   });
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
+
+export const removeFromCart = (productId) => (dispatch, getState) => {
+  dispatch({ type: CART_REMOVE_ITEM, payload: productId });
   localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
 };
