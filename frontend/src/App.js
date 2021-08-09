aea12651c8ce811a00d5794f8bdcbe52411ffd48
arieltecay <arieltecay@gmail.com>
2021-08-08 22:41:16 -0300

    actions, constants, reducers of signin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,21 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { BrowserRouter, Route } from "react-router-dom";
 import { Link } from "react-router-dom";
 import CartScreen from "./screens/CartScreen";
 import HomeScreen from "./screens/HomeScreen";
+import { signout } from "./actions/userActions";
 import ProductScreen from "./screens/ProductScreen";
 import SigninScreen from "./screens/SigninScreen";
 
 function App() {
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  const dispatch = useDispatch();
+  const signoutHandler = () => {
+    dispatch(signout());
+  };
   return (
     <BrowserRouter>
       <div>
@@ -26,7 +33,22 @@ function App() {
                   <span className="badge">{cartItems.length} </span>
                 )}
               </Link>
-              <Link to="/signin">Sign In</Link>
+              {userInfo ? (
+                <div className="dropdown">
+                  <Link to="#">
+                    {userInfo.name} <i className="fa fa-caret-down"></i>{" "}
+                  </Link>
+                  <ul className="dropdown-content">
+                    <li>
+                      <Link to="#signout" onClick={signoutHandler}>
+                        Sign Out
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              ) : (
+                <Link to="/signin">Sign In</Link>
+              )}
             </div>
           </header>
           <main>
