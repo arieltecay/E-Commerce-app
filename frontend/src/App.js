8a208882ce9bb29519e4e4bc27ed18e0122f5252
arieltecay <arieltecay@gmail.com>
2021-08-09 23:20:28 -0300

    Shipping screen and working with redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import { signout } from "./actions/userActions";
 import ProductScreen from "./screens/ProductScreen";
 import SigninScreen from "./screens/SigninScreen";
 import RegisterScreen from "./screens/RegisterScreen";
+import ShippingAddressScreen from "./screens/ShippingAddressScreen";
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -57,6 +58,7 @@ function App() {
             <Route path="/product/:id" component={ProductScreen} exact />
             <Route path="/signin" component={SigninScreen} exact />
             <Route path="/register" component={RegisterScreen}></Route>
+            <Route path="/shipping" component={ShippingAddressScreen}></Route>
             <Route path="/" component={HomeScreen} exact />
           </main>
           <footer className="row center">
--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,4 +1,8 @@
-import { CART_ADD_ITEM,CART_REMOVE_ITEM } from "../constants/CartConstants";
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  CART_SAVE_SHIPPING_ADDRESS,
+} from "../constants/CartConstants";
 import Axios from "axios";
 
 export const addToCart = (productId, qty) => async (dispatch, getState) => {
@@ -20,4 +24,9 @@ export const addToCart = (productId, qty) => async (dispatch, getState) => {
 export const removeFromCart = (productId) => (dispatch, getState) => {
   dispatch({ type: CART_REMOVE_ITEM, payload: productId });
   localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
+
+export const saveShippingAddress = (data) => (dispatch) => {
+  dispatch({ type: CART_SAVE_SHIPPING_ADDRESS, payload: data });
+  localStorage.setItem("shippingAddress", JSON.stringify(data));
 };
