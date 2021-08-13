877c32bd82d37fdc5b2cea6fbabce1e122f3d115
arieltecay <arieltecay@gmail.com>
2021-08-13 16:31:14 -0300

    implement order in backend

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import userRouter from "./routers/userRouter.js";
 import dotenv from "dotenv";
 import productRouter from "./routers/productRouter.js";
-
+import orderRouter from "./routers/orderRouter.js";
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -17,6 +17,7 @@ mongoose.connect(process.env.MONGODB_URL || "", {
 
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
 
 app.get("/", (req, res) => {
   res.send("Server is ready");
